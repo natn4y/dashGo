fbd1570aafdf77a1f24fb4d89b3699e23020185d
Natanael Araújo <natanael@hotmail.co.jp>
2022-02-22 01:27:20 -0300

    ☕ Update: Componentização da página SignIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,8 @@
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Input,
-  Stack,
-} from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { Input } from "../components/Forms/Input";
 
 const SignIn: NextPage = () => {
   return (
@@ -28,35 +22,8 @@ const SignIn: NextPage = () => {
         flexDir="column"
       >
         <Stack spacing="4">
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              focusBorderColor="pink.500"
-              bg="gray.900"
-              _hover={{ bg: "gray.900" }}
-              variant="filled"
-              size="lg"
-            />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="password">Senha</FormLabel>
-
-            <Input
-              id="password"
-              name="password"
-              type="password"
-              focusBorderColor="pink.500"
-              bg="gray.900"
-              _hover={{ bg: "gray.900" }}
-              variant="filled"
-              size="lg"
-            />
-          </FormControl>
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
         </Stack>
 
         <Button
