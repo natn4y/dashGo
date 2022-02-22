a4472f6850cd64fd62bd2b975e571cca89a19fcc
Natanael Araújo <natanael@hotmail.co.jp>
2022-02-22 20:37:03 -0300

    📂 Componente Header

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,15 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+export function Profile() {
+  return (
+    <Flex align="center">
+      <Box mr="4" textAlign="right">
+        <Text>Diego Fernandes</Text>
+        <Text color="gray.300" fontSize="small">
+          @diego_fernandes
+        </Text>
+      </Box>
+      <Avatar size="md" name="Diego Fernandes" />
+    </Flex>
+  );
+}
