a09dd92917f0259e2c32d8baad40b3954bd4c354
Natanael Araújo <natanael@hotmail.co.jp>
2022-02-27 00:43:19 -0300

    ❗️ Fix: Correção onClose da Sidebar versão mobile

--- src/contexts/SidebarDrawerContext.tsx
+++ src/contexts/SidebarDrawerContext.tsx
@@ -1,29 +1,28 @@
-import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
-import { useRouter } from "next/router";
-import { createContext, ReactNode, useContext, useEffect } from "react";
+import { createContext, ReactNode, useContext, useEffect } from 'react'
+import { useDisclosure, UseDisclosureReturn } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 
 interface SidebarDrawerProviderProps {
   children: ReactNode;
 }
 
-type SidebarDrawerContextData = UseDisclosureReturn
+type SidebarDrawerContextData = UseDisclosureReturn;
 
 const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
 
 export function SidebarDrawerProvider({ children }: SidebarDrawerProviderProps) {
-  const disclosure = useDisclosure();
-  const { onClose } = disclosure;
-  const router = useRouter();
+  const disclosure = useDisclosure()
+  const router = useRouter()
 
   useEffect(() => {
-    onClose
-  }, [onClose, router.asPath]);
+    disclosure.onClose()
+  }, [router.asPath])
 
   return (
     <SidebarDrawerContext.Provider value={disclosure}>
       {children}
     </SidebarDrawerContext.Provider>
-  );
+  )
 }
 
 export const useSidebarDrawer = () => useContext(SidebarDrawerContext);
