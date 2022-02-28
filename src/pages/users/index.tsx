6b0aeb37865f9bee46c2f4e383130a463a401c60
Natanael Araújo <natanael@hotmail.co.jp>
2022-02-28 09:48:22 -0300

    🔧 Configuração do axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -8,6 +8,7 @@ import { useQuery } from 'react-query';
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from '../../components/Sidebar';
+import { api } from '../../services/api';
 
 type User = {
   id: number;
@@ -18,8 +19,7 @@ type User = {
 
 export default function UserList() {
   const { data, isLoading, isFetching, error, refetch } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
+    const { data } = await api.get('users');
 
     const users = data.users.map((user: User) => {
       return {
